850f12119fbe81c637741818e72462198314f352
Borisovsky Victor <vyuborisovskiy@stud.eltech.ru>
2020-04-28 02:06:57 +0600

    orgin

---
+++ cmake-build-debug/CMakeFiles/bmp_processing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmp_processing.dir/main.c.o"
+  "bmp_processing"
+  "bmp_processing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bmp_processing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
