d2efdcd82aed3a3ecc5595778ac04d62f666b2f6
Borisovsky Victor <vyuborisovskiy@stud.eltech.ru>
2020-05-16 00:45:45 +0600

    работает

--- PrintHelp.c
+++ PrintHelp.c
@@ -4,7 +4,7 @@
 void PrintHelp(){
     printf("    BMP_PROCESSING PROGRAM    \n");
     printf("1) --line-l - arguments: x1 y1 x2 y2 thickness color\n");
-    printf("COLOR LIST: [red, green, pink, orange, blue, purple, black, white, brown]\n");
+    printf("COLOR LIST: [red, green, pink, orange, blue, purple, black, white, brown, yellow]\n");
     printf("2) --inv_1-i - arguments: x0 y0 radius\n");
     printf("3) --inv_2-I - arguments: x1 y1 x2 y2\n");
     printf("4) --crop-c - arguments: x1 y1 x2 y2\n");
